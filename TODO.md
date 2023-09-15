e228e2e41294c1b34d14b55fda74e3363a0ab10d
0xFOSSMan <domi382@outlook.com>
2023-09-15 12:23:03 +0200

    Fixed double entry bug

--- TODO.md
+++ TODO.md
@@ -2,5 +2,6 @@
 
 - [x] List all apps
 - [X] Filter Games out of the list
+- [X] Fixed the double entry bug
 - [ ] Add App Icon to list
 - [ ] Make a nice design
--- app/src/main/java/app/fossman/gamelauncher/MainActivity.kt
+++ app/src/main/java/app/fossman/gamelauncher/MainActivity.kt
@@ -12,13 +12,14 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.ClickableText
-import androidx.compose.material3.Divider
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -30,61 +31,77 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column (
-                verticalArrangement = Arrangement.SpaceAround
-            ){
-                Text(text = "GameLauncher", modifier = Modifier.align(Alignment.CenterHorizontally))
-                val pm: PackageManager = packageManager
-                val sb = StringBuilder()
-                val appsList = ArrayList<ApplicationInfo>()
-                val i = Intent(Intent.ACTION_MAIN, null)
-                val allApps = pm.queryIntentActivities(i, 0)
-                val tag = "GameLauncher"
+            Box (){
+                Column(
+                    verticalArrangement = Arrangement.SpaceAround
+                ) {
+                    Text(
+                        text = "GameLauncher",
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally)
+                    )
+                    val pm: PackageManager = packageManager
+                    val sb = StringBuilder()
+                    val appsList = ArrayList<ApplicationInfo>()
+                    val gamePackageList = ArrayList<String>()
+                    val i = Intent(Intent.ACTION_MAIN, null)
+                    val allApps = pm.queryIntentActivities(i, 0)
+                    val tag = "GameLauncher"
 
-                i.addCategory(Intent.CATEGORY_LAUNCHER)
-                //https://stackoverflow.com/questions/31600256/how-to-check-if-the-app-is-game-or-not-programmatically/31600301#31600301
-                fun packageIsGame(context: Context, packageName: String): Boolean {
-                    return try {
-                        val info: ApplicationInfo =
-                            context.packageManager.getApplicationInfo(packageName, 0)
-                        info.category == ApplicationInfo.CATEGORY_GAME
-                    } catch (e: PackageManager.NameNotFoundException) {
-                        false
+                    i.addCategory(Intent.CATEGORY_LAUNCHER)
+                    //https://stackoverflow.com/questions/31600256/how-to-check-if-the-app-is-game-or-not-programmatically/31600301#31600301
+                    fun packageIsGame(context: Context, packageName: String): Boolean {
+                        return try {
+                            val info: ApplicationInfo =
+                                context.packageManager.getApplicationInfo(packageName, 0)
+                            info.category == ApplicationInfo.CATEGORY_GAME
+                        } catch (e: PackageManager.NameNotFoundException) {
+                            false
+                        }
                     }
-                }
 
-                for (ri in allApps) {
-                    val app = ApplicationInfo()
-                    app.name = ri.loadLabel(pm).toString()
-                    app.packageName = ri.activityInfo.packageName
-                    if(packageIsGame(LocalContext.current, app.packageName)){
-                        if (!appsList.contains(app)){
-                            appsList.add(app)
-                            Log.v(tag, "Added App: ".plus(app.packageName))
+                    for (ri in allApps) {
+                        val app = ApplicationInfo()
+                        app.name = ri.loadLabel(pm).toString()
+                        app.packageName = ri.activityInfo.packageName
+                        if (packageIsGame(LocalContext.current, app.packageName)) {
+                            if (gamePackageList.contains(app.packageName)) {
+                                Log.v(tag, "duplicate package ".plus(app.packageName))
+                            } else {
+                                appsList.add(app)
+                                Log.v(tag, "Added App: ".plus(app.packageName).plus(" ").plus(pm.getLaunchIntentForPackage(app.packageName)))
+                            }
+                            gamePackageList.add(app.packageName)
+                        }
+                    }
+
+                    sb.append(appsList.size)
+                    sb.append(" apps loaded")
+                    Toast.makeText(
+                        this@MainActivity,
+                        sb.toString(),
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                    LazyColumn {
+                        items(appsList) { app ->
+                            ClickableText(
+                                AnnotatedString(app.name),
+                                modifier = Modifier
+                                    .padding(15.dp)
+                                    .align(Alignment.CenterHorizontally)
+                                    .fillMaxWidth(),
+                                onClick = {
+                                    val launchIntent: Intent? =
+                                        pm.getLaunchIntentForPackage(app.packageName)
+                                    startActivity(launchIntent)
+                                }
+                            )
+                            Divider()
                         }
                     }
                 }
-
-                sb.append(appsList.size)
-                sb.append(" apps loaded")
-                Toast.makeText(
-                    this@MainActivity,
-                    sb.toString(),
-                    Toast.LENGTH_SHORT
-                ).show()
-
-                LazyColumn {
-                    items(appsList) { app ->
-                        ClickableText(AnnotatedString(app.name), modifier = Modifier.padding(15.dp), onClick ={
-                            val launchIntent: Intent? = pm.getLaunchIntentForPackage(app.packageName)
-                            startActivity(launchIntent)
-                        })
-                        Divider()
-                    }
-                }
-
             }
         }
-
     }
 }
